84d9eaa1cb5a0080a5724105f1ded4955a8fa2c3
Axel Pauwels <axel.pauwels@innomedio.be>
2020-11-26 14:23:10 +0100

    Chapter 4

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,12 +6,29 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function homepage()
     {
-        return new Response('What a controller !');
+        return new Response('What a controller ! With annotation routes instead of using routes.yaml');
     }
 
+    /**
+     * @Route("/questions/{slug}")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show "%s"',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
+    }
 }
