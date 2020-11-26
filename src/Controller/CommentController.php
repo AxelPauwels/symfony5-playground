4caa071c4714b0396670b1615a82d54e1203e5d4
Axel Pauwels <axel.pauwels@innomedio.be>
2020-11-26 16:46:27 +0100

    Chapter 16

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,18 +14,24 @@ class CommentController extends AbstractController
 
     // methods="POST"   will only allow POST requests
     // <\d+>            will only allow 'id' to be a decimal
-    // <up\down>        will only allow 'direction' to be "up" or "down"
+    // <up|down>        will only allow 'direction' to be "up" or "down"
     /**
-     * @Route("/comments/{id<\d+>}/vote/{direction<up\down>}", methods="POST")
+     * @Route("/comments/{id<\d+>}/vote/{direction<up|down>}", methods="POST")
+     * @param $id
+     * @param $direction
+     * @param LoggerInterface $logger
+     * @return JsonResponse
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger):JsonResponse
     {
         //todo use id to query db
 
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
-            $currentVoteCount = random_int(0, 5);
+            $currentVoteCount = rand(0, 5);
+            $logger->info('Voting down!');
         }
 
         // -> Returns with Content-Type: application/json
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
@@ -15,8 +16,14 @@ class QuestionController extends AbstractController
      * @Route("/", name="app_homepage")
      * @return Response
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
+
+        // the "return $this->render" at the bottom just uses symfony 'Services'..."Objects that do something.."
+        // it replaces this:
+        // $html = $twigEnvironment->render('question/homepage.html.twig');
+        // return new Response($html);
+
         return $this->render('question/homepage.html.twig');
     }
 
@@ -27,14 +34,13 @@ class QuestionController extends AbstractController
      */
     public function show($slug)
     {
-
         $answers = [
             'This is answer 1',
             'This is answer 2',
             'This is answer 3'
         ];
 
-//        dd($slug, $this);
+        // dd($slug, $this);
         dump($slug, $this);
 
         return $this->render('question/show.html.twig', [
