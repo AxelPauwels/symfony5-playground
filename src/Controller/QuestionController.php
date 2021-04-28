abe274a212fa300f2e85b90eb3a46d58e02cce14
Axel Pauwels <axel.pauwels@innomedio.be>
2021-04-28 11:49:03 +0200

    Doctrine, Symfony & the Database - Chapter 9

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -79,7 +79,7 @@ EOF
      * @param MarkdownHelper $markdownHelper
      * @return Response
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
 //        dump($this->getParameter('cache_adapter'));
 //        throw new \Exception('Bad stuff happened');
@@ -87,6 +87,14 @@ EOF
 
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode');
+        }
+
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
         }
 
         $answers = [
